96ad48f274155018c88618e27f22f6b3f170bff4
abruno92 <abruno92@github.com>
2021-01-01 13:39:50 +0100

    added all crud for SKATUSER

--- Skat/routes/skatUser.js
+++ Skat/routes/skatUser.js
@@ -9,7 +9,7 @@ const pool = mysql.createPool({
     database: 'skat'
 });
 
-// Get All Rows
+// Get all Skat users
 router.get('/', (req, res) => {
     req.socket.setTimeout(10000);
     pool.getConnection((err, connection) => {
@@ -27,4 +27,64 @@ router.get('/', (req, res) => {
     });
 });
 
+// Create new Skat user
+router.post('', (req, res) => {
+    req.socket.setTimeout(10000);
+    pool.getConnection((err, connection) => {
+        if(err) throw err
+
+        const params = req.body
+        connection.query('INSERT INTO SkatUser SET ?', params, (err) => {
+            connection.release()
+
+            if(!err) {
+                res.send(`Entry was succesfully added`)
+            }else{
+                console.log(err)
+            }
+        })
+
+        console.log('The data just added is at ');
+    });
+});
+
+// Update Existing Skat User
+router.put('', (req, res) => {
+    req.socket.setTimeout(10000);
+    pool.getConnection((err, connection) => {
+        if(err) throw err
+
+        const {Id, Address} = req.body;
+
+        connection.query('UPDATE SkatUser SET SkatUser = ? WHERE Id = ?', [SkatUser, Id], (err) => {
+            connection.release()
+
+            if(!err) {
+                res.send(`Entry was succesfully updated`)
+            }else{
+                console.log(err)
+            }
+        })
+    });
+});
+
+// Delete skat user by ID
+router.delete('/:id', (req, res) => {
+    req.socket.setTimeout(10000);
+    pool.getConnection((err, connection) => {
+        if(err) throw err
+
+        connection.query('DELETE FROM Address WHERE Id = ?', [req.params.id], (err) => {
+            connection.release() 
+
+            if(!err) {
+                res.send(`Entry ${[req.params.id]} was successfully deleted`)
+            }else{
+                console.log(err)
+            }
+        })
+    });
+});
+
 module.exports = router;
+
